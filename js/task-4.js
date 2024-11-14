742a94cde83db254c8847a271b853946765cb303
julia <julia.zhyvylo@asila.mk>
2024-11-14 20:25:28 +0100

    task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+const handleFormSubmit = (e) => {
+  e.preventDefault();
+
+  const email = e.target.elements.email.value;
+  const password = e.target.elements.password.value;
+
+  if (!email.trim().length || !password.trim().length)
+    return alert("All form fields must be filled in");
+
+  const formObj = {
+    email,
+    password
+  }
+
+  console.log(formObj);
+
+  form.reset();
+};
+
+form.addEventListener("submit", handleFormSubmit);
